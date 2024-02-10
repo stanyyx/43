0e655ef7522a3e28c321951974033a2953330cb0
stanyyx <125898976+stanyyx@users.noreply.github.com>
2024-02-10 19:24:37 +0300

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from logistic.models import Product, Stock,StockProduct
